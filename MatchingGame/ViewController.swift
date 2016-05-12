d3845635052f139fb64ee1daaa1622d677da6286
lmx0412 <LiuMengxuan@liumengxuandeimac.local>
2016-05-13 01:02:03 +0800

    add some images

--- MatchingGame/ViewController.swift
+++ MatchingGame/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image_background: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
