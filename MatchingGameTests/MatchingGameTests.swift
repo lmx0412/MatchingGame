c906e72308c76dd42d1d5331a90c3962639739dd
lmx0412 <LiuMengxuan@liumengxuandeimac.local>
2016-05-12 21:31:34 +0800

    Initial Commit

---
+++ MatchingGameTests/MatchingGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  MatchingGameTests.swift
+//  MatchingGameTests
+//
+//  Created by 刘孟轩 on 16/5/12.
+//  Copyright © 2016年 刘孟轩. All rights reserved.
+//
+
+import XCTest
+@testable import MatchingGame
+
+class MatchingGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
