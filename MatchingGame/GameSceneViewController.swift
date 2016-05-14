9c6da833dd9259db7bf88abfe8e8f7343edfdd4d
lmx0412 <LiuMengxuan@liumengxuandeimac.local>
2016-05-14 02:11:37 +0000

    add buttons in GameSceneViewController and their constraints.

---
+++ MatchingGame/GameSceneViewController.swift
@@ -0,0 +1,62 @@
+//
+//  GameSceneViewController.swift
+//  MatchingGame
+//
+//  Created by 刘孟轩 on 16/5/13.
+//  Copyright © 2016年 刘孟轩. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GameSceneViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    @IBOutlet weak var button11: UIButton!
+    @IBOutlet weak var button12: UIButton!
+    @IBOutlet weak var button13: UIButton!
+    @IBOutlet weak var button14: UIButton!
+    @IBOutlet weak var button15: UIButton!
+    @IBOutlet weak var button16: UIButton!
+    @IBOutlet weak var button17: UIButton!
+    @IBOutlet weak var button18: UIButton!
+    @IBOutlet weak var button19: UIButton!
+    @IBOutlet weak var button20: UIButton!
+    @IBOutlet weak var button21: UIButton!
+    @IBOutlet weak var button22: UIButton!
+    @IBOutlet weak var button23: UIButton!
+    @IBOutlet weak var button24: UIButton!
+    @IBOutlet weak var button25: UIButton!
+    @IBOutlet weak var button26: UIButton!
+    @IBOutlet weak var button27: UIButton!
+    @IBOutlet weak var button28: UIButton!
+    @IBOutlet weak var button29: UIButton!
+    @IBOutlet weak var button30: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func buttonTap(sender: UIButton) {
+    }
+
+    
+    
+    
+    
+}
