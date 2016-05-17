463b291ee794724468d236607f55af2e89992eb4
lmx0412 <LiuMengxuan@liumengxuandeimac.local>
2016-05-17 01:19:33 +0000

    basically have finished all the game procedure,next step is to add shuffle func .

--- MatchingGame/GameSceneViewController.swift
+++ MatchingGame/GameSceneViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class GameSceneViewController: UIViewController {
     
+    @IBOutlet weak var GameSceneLabel: UILabel!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -42,7 +43,13 @@ class GameSceneViewController: UIViewController {
     @IBOutlet weak var button29: UIButton!
     @IBOutlet weak var button30: UIButton!
     
-    
+    var isDisable = false
+    var pictureFlipped = -1
+    var picture1: UIButton = UIButton()
+    var picture2: UIButton = UIButton()
+    var ismatch = false
+    var stepCount = 0
+    var matchCount = 0
     var picture:NSArray = [
         UIImage(named: "icon01.png")!,
         UIImage(named: "icon01.png")!,
@@ -86,12 +93,59 @@ class GameSceneViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func buttonTap(sender: UIButton) {
-        var selectImage :UIImage = self.picture[sender.tag-1] as! UIImage
-        
-        sender.setImage(selectImage, forState: UIControlState.Normal)
+        self.stepCount += 1
+        if(isDisable == true){
+            return
+        }
+        if(self.pictureFlipped >= 0 && self.pictureFlipped != sender.tag){
+            
+            let lastImage :UIImage = self.picture[self.pictureFlipped-1] as! UIImage
+            self.picture2 = sender as UIButton
+            let selectImage :UIImage = self.picture[sender.tag-1] as! UIImage
+            sender.setImage(selectImage, forState: UIControlState.Normal)
+            isDisable = true
+            self.pictureFlipped = -1
+            if(lastImage == selectImage){
+                self.picture1.enabled = false
+                self.picture2.enabled = false
+                //self.picture1.userInteractionEnabled = true
+                //self.picture2.userInteractionEnabled = true
+                self.picture1.hidden = true
+                self.picture2.hidden = true
+                ismatch = true
+                self.matchCount += 1
+            }
+            if(!ismatch){
+                NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(GameSceneViewController.resetPicture), userInfo: nil, repeats: false)
+            }
+            else{
+                resetPicture()
+            }
+            if(self.matchCount == 15){
+                gameFinish()
+            }
+        }
+        else{
+            self.pictureFlipped = sender.tag
+            self.picture1 = sender as UIButton
+            let selectImage :UIImage = self.picture[sender.tag-1] as! UIImage
+            sender.setImage(selectImage, forState: UIControlState.Normal)
+        }
     }
 
+    func resetPicture(){
+        if(!ismatch){
+            self.picture1.setImage(nil, forState: UIControlState.Normal)
+            self.picture2.setImage(nil, forState: UIControlState.Normal)
+        }
+        isDisable = false
+        ismatch = false
+    }
     
+    func gameFinish(){
+        self.stepCount = self.stepCount/2
+        self.GameSceneLabel.text = "You win with \(self.stepCount)steps"
+    }
     
     
     
